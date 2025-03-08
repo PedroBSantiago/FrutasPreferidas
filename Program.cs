af278a4c144874b78d022676cbaab64dbbd77f92
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-07 22:34:30 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.WriteLine(@"
+_____My favorite fruits_____");
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine(@"
+1.Pera");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(@"
+2.Maçã Verde");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(@"
+3.Melancia");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine(@"
+4.melão");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine(@"
+5.Pitaya");
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.ResetColor();
+Console.WriteLine("\n");
